28450faa07ba1879ad84926d8594fdc401febdc3
Jarrett Keifer <jkeifer0@gmail.com>
2025-08-13 16:54:28 -0700

    use nix flake for hugo

---
+++ flake/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devshell}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            hugo
+          ];
+        };
+      }
+    );
+}
